d9212757ee116ebf1547e96754c4525e1858320e
Josh Brooks <defaultjosh@hotmail.com>
2020-05-17 12:42:35 -0400

    updated for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ var mysql = require("mysql");
 
 //1)create connection object
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "pw10Mysql",
-    database: "burgers_db"
+    user: "v3f7xio2vshoxux5",
+    password: "proii5vb21hwrb9b",
+    database: "owfhlxqo28cb1k00"
 })
 
 //2)connect
